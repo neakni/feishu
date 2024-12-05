0716184ee353c2bfa2d2798e7a45db2e1d3252be
wuqiang <997720994@qq.com>
2024-12-05 10:26:36 +0800

    feishu config change

--- config/feishu.php
+++ config/feishu.php
@@ -2,6 +2,6 @@
 
 return [
     'app_id'  => env('FEISHU_APPID', ''),         // AppID
-    'secret'  => env('FEISHU_SECRET', ''),    // AppSecret
+    'app_secret'  => env('FEISHU_SECRET', ''),    // AppSecret
     'redirect_uri'  => env('FEISHU_REDIRECT_URI', ''),    // RedirectUri
 ];
